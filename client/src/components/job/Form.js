e09e6227cd90bce664e386af41d5308ad41827b3
Marcus Couto <markitovc@gmail.com>
2020-10-03 16:15:24 -0300

    include react-hooks injob Form

--- client/src/components/job/Form.js
+++ client/src/components/job/Form.js
@@ -1,55 +1,75 @@
-import React, { Component } from 'react';
-import { createJob } from '../../requests'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-export class JobForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: '', description: ''};
-  }
+import { useMutation } from "@apollo/react-hooks";
+import { addJob } from "../../graphql/queries";
 
-  handleChange(event) {
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
+export const JobForm = () => {
+  const [job, setJob] = useState({});
+  const history = useHistory();
 
-  handleClick(event) {
+  const [createJob] = useMutation(addJob, {
+    onCompleted: ({ createJob }) => {
+      history.push(`/jobs/${createJob.id}`);
+    },
+  });
+
+  const handleChange = (event) => {
+    const auxValues = { ...job };
+    auxValues[event.target.name] = event.target.value;
+    setJob(auxValues);
+  };
+
+  const handleClick = (event) => {
     event.preventDefault();
-    const { title, description } = this.state
-    createJob({ title, description}).then((job)=> {  
-      this.props.history.push(`/jobs/${job.createJob.id}`); 
-    })
-    console.log('should post a new job:', this.state);
-  }
+    const { title, description } = job;
+    createJob({
+      variables: {
+        input: { title, description },
+      },
+    });
+  };
 
-  render() {
-    const {title, description} = this.state;
-    return (
-      <div>
-        <h1 className="title">New Job</h1>
-        <div className="box">
-          <form>
-            <div className="field">
-              <label className="label">Title</label>
-              <div className="control">
-                <input className="input" type="text" name="title" value={title}
-                  onChange={this.handleChange.bind(this)} />
-              </div>
+  const { title, description } = job;
+
+  return (
+    <div>
+      <h1 className="title">New Job</h1>
+      <div className="box">
+        <form>
+          <div className="field">
+            <label className="label">Title</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange}
+              />
             </div>
-            <div className="field">
-              <label className="label">Description</label>
-              <div className="control">
-                <textarea className="input" style={{height: '10em'}}
-                  name="description" value={description} onChange={this.handleChange.bind(this)} />
-              </div>
+          </div>
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <textarea
+                className="input"
+                style={{ height: "10em" }}
+                name="description"
+                value={description}
+                onChange={handleChange}
+              />
             </div>
-            <div className="field">
-              <div className="control">
-                <button className="button is-link" onClick={this.handleClick.bind(this)}>Submit</button>
-              </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-link" onClick={handleClick}>
+                Submit
+              </button>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
